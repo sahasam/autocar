c7b7722925fb3b7cfa0baacb4819bbc1f73cd491
sahasam <munamalasahas@gmail.com>
2018-07-05 17:07:35 +0000

    Fix BNO055 and add Makefile

---
+++ Makefile
@@ -0,0 +1,10 @@
+HOST=127.0.0.1
+TEST_PATH=./
+
+clean:
+	find . -name '*.pyc' -exec rm --force {} +
+	find . -name '*.pyo' -exec rm --force {} +
+	-exec sudo python cleanup.py
+
+main:
+	-exec sudo python main.py
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import time
 import ldrhandler
+import motorhandler
+import bnohandler
 
 try:
     while True:
-        print('working')
-        time.sleep(1)
+        motorhandler.forwardDrive()
+        print(bnohandler.getHeading())
+        time.sleep(0.01)
 except  KeyboardInterrupt:
     motorhandler.GPIO.cleanup()
     ldrhandler.cleanup()
